d3eeee853f2e7225fd516426daced80e53ce41c5
Nathanael Martins Wink <131195464+NathanpznBR@users.noreply.github.com>
2023-05-09 08:28:14 -0400

    Update parser.cpp

--- TP/Parte2/parser.cpp
+++ TP/Parte2/parser.cpp
@@ -2,343 +2,368 @@
 
 Parser::Parser(string input)
 {
-	scanner = new Scanner(input);
+    scanner = new Scanner(input);
 }
 
-void
-Parser::advance()
+void Parser::advance()
 {
-	lToken = scanner->nextToken();
+    lToken = scanner->nextToken();
 }
 
-void
-Parser::match(int t)
+void Parser::match(int t)
 {
-	if (lToken->name == t || lToken->attribute == t)
-		advance();
-	else
-		error("Erro inesperado");
+    if (lToken->name == t || lToken->attribute == t)
+        advance();
+    else
+        error("Erro inesperado");
 }
 
-void
-Parser::run()
+void Parser::run()
 {
-	advance();	
+    advance();
 
-	program();
-	
-	cout << "Compilação encerrada com sucesso!\n";
+    program();
+
+    cout << "Compilação encerrada com sucesso!\n";
 }
 // .1 Program -> MainClass (ClassDeclaration) * EOF
-void
-Parser::program()
+void Parser::program()
 {
-	//TODO
+    // TODO
     mainClass();
 
-	while(lToken->name == CLASS) {
-		classDeclaration();
-	}
+    while (lToken->name == CLASS)
+    {
+        classDeclaration();
+    }
 
-	match(EOF);
+    match(EOF);
 }
 
-//Continuar....
-
-void
-Parser::match(int t)
+void Parser::match(int t)
 {
-	if (lToken->name == t || lToken->attribute == t)
-		advance();
-	else
-		error("Erro inesperado");
+    if (lToken->name == t || lToken->attribute == t)
+        advance();
+    else
+        error("Erro inesperado");
 }
 
 // 2. MainClass -> class ID { public static void main(String[] ID){ Statement }}
-void
-Parser::mainClass()
+void Parser::mainClass()
 {
     match(CLASS);
-	match(ID);
-	match(LCHAVE);
-	match(PUBLIC);
-	match(STATIC);
-	match(VOID);
-	match(MAIN);
-	match(LPAREN);
-	match(STRING);
-	match(LCOLCH);
-	match(RCOLCH);
-	match(ID);
-	match(RPAREN);
-	match(LCHAVE);
-	statement();
-	match(RCHAVE);
-	match(RCHAVE);
+    match(ID);
+    match(LCHAVE);
+    match(PUBLIC);
+    match(STATIC);
+    match(VOID);
+    match(MAIN);
+    match(LPAREN);
+    match(STRING);
+    match(LCOLCH);
+    match(RCOLCH);
+    match(ID);
+    match(RPAREN);
+    match(LCHAVE);
+    statement();
+    match(RCHAVE);
+    match(RCHAVE);
 }
 // 3. ClassDeclaration → class ID (extends ID)? { (VarDeclaration)* (MethodDeclaration)*
-}
+
 void Parser::classDeclaration()
 {
-	match(CLASS);
-	match(ID);
-	if(lToken->name == EXTENDS) {
-		match(EXTENDS);
-		match(ID);
-	}
-	match(LBRACE);
-	while(lToken->name == BOOLEAN || lToken->name == INT || lToken->name == ID) {
-		varDeclaration();
-	}
-	while(lToken->name == PUBLIC) {
-		methodDeclaration();
-	}
-	
-	match(RBRACE);
+    match(CLASS);
+    match(ID);
+    if (lToken->name == EXTENDS)
+    {
+        match(EXTENDS);
+        match(ID);
+    }
+    match(LCOLCH);
+    while (lToken->name == BOOLEAN || lToken->name == INT || lToken->name == ID)
+    {
+        varDeclaration();
+    }
+    while (lToken->name == PUBLIC)
+    {
+        methodDeclaration();
+    }
 
+    match(RPAREN);
 }
 // 4. VarDeclaration -> Type ID ;
-void
-Parser::varDeclaration()
+void Parser::varDeclaration()
 {
-	type();
-	match(ID);
-	match(PONTVIR);
+    type();
+    match(ID);
+    match(PONTVIR);
 }
-
-void
-Parser::methodDeclaration()
+// 5. MethodDeclaration -> public Type ID ( (Type ID) * ) { (VarDeclaration)* (Statement)* return Expression ; }
+void Parser::methodDeclaration()
 {
-	match(PUBLIC);
-	type();
-	match(ID);
-	match(LPAREN);
-	if (lToken->name == BOOLEAN || lToken->name == INT
-		|| lToken->name == ID) {
-		type();
-		match(ID);
-		while (lToken->name == VIRGULA) {
-			match(VIRGULA);
-			type();
-			match(ID);
-		}
-	}
-	match(RPAREN);
-	match(LCHAVE);
-	while (lToken->name == BOOLEAN || lToken->name == INT
-		   || lToken->name == ID) {
-		varDeclaration();
-	}
-	while (lToken->name != RETURN) {
-		statement();
-	}
-	match(RETURN);
-	expression();
-	match(PONTVIR);
-	match(RCHAVE);
+    match(PUBLIC);
+    type();
+    match(ID);
+    match(LPAREN);
+    if (lToken->name == BOOLEAN || lToken->name == INT || lToken->name == ID)
+    {
+        type();
+        match(ID);
+        while (lToken->name == VIRGULA)
+        {
+            match(VIRGULA);
+            type();
+            match(ID);
+        }
+    }
+    match(RPAREN);
+    match(LCHAVE);
+    while (lToken->name == BOOLEAN || lToken->name == INT || lToken->name == ID)
+    {
+        varDeclaration();
+    }
+    while (lToken->name != RETURN)
+    {
+        statement();
+    }
+    match(RETURN);
+    expression();
+    match(PONTVIR);
+    match(RCHAVE);
 }
-
-void
-Parser::type()
+// 6. Type -> int [ ] | boolean | int | ID [ ]
+void Parser::type()
 {
-	if (lToken->name == INT) {
-		match(INT);
-		if (lToken->name == LBRACKET) {
-			match(LBRACKET);
-			match(RBRACKET);
-		}
-	} else if (lToken->name == BOOLEAN) {
-		match(BOOLEAN);
-	} else if (lToken->name == ID) {
-		match(ID);
-		if (lToken->name == LBRACKET) {
-			match(LBRACKET);
-			match(RBRACKET);
-		}
-	} else {
-		error("Tipo esperado");
-	}
+    if (lToken->name == INT)
+    {
+        match(INT);
+        if (lToken->name == LCOLCH)
+        {
+            match(LCOLCH);
+            match(RCOLCH);
+        }
+    }
+    else if (lToken->name == BOOLEAN)
+    {
+        match(BOOLEAN);
+    }
+    else if (lToken->name == ID)
+    {
+        match(ID);
+        if (lToken->name == LCOLCH)
+        {
+            match(LCOLCH);
+            match(RCOLCH);
+        }
+    }
+    else
+    {
+        error("Tipo esperado");
+    }
 }
-
+// 7. Statement -> { (Statement)* } | if ( Expression ) Statement (else Statement)? 
+// | while ( Expression ) Statement | System.out.println ( Expression ) ; 
+// | ID = Expression ; | ID [ Expression ] = Expression ;
 void Parser::statement()
 {
-    switch(lToken->name)
+    switch (lToken->name)
     {
-        case '{':
-            match('{');
-            while(lToken->name != '}')
-                statement();
-            match('}');
-            break;
-        case IF:
-            match(IF);
-            match('(');
-            expression();
-            match(')');
+    case LCHAVE:
+        match(LCHAVE);
+        while (lToken->name != RCHAVE)
             statement();
-            if(lToken->name == ELSE)
-            {
-                match(ELSE);
-                statement();
-            }
-            break;
-        case WHILE:
-            match(WHILE);
-            match('(');
-            expression();
-            match(')');
+        match(RCHAVE);
+        break;
+    case IF:
+        match(IF);
+        match(LPAREN);
+        expression();
+        match(RPAREN);
+        statement();
+        if (lToken->name == ELSE)
+        {
+            match(ELSE);
             statement();
-            break;
-        case SYSTEM:
-            match(SYSTEM);
-            match('.');
-            match(OUT);
-            match('.');
-            match(PRINTLN);
-            match('(');
+        }
+        break;
+    case WHILE:
+        match(WHILE);
+        match(LPAREN);
+        expression();
+        match(RPAREN);
+        statement();
+        break;
+    case SYSTEM:
+        match(SYSTEM);
+        match(PONTO);
+        match(OUT);
+        match(PONTO);
+        match(PRINTLN);
+        match(LPAREN);
+        expression();
+        match(RPAREN);
+        match(PONTVIR);
+        break;
+    case ID:
+        match(ID);
+        if (lToken->name == EQUAL) // ID = Expression ;
+        {
+            match(EQUAL);
             expression();
-            match(')');
-            match(';');
-            break;
-        case ID:
-            match(ID);
-            if(lToken->name == '=') // ID = Expression ;
-            {
-                match('=');
-                expression();
-                match(';');
-            }
-            else if(lToken->name == '[') // ID [ Expression ] = Expression ;
-            {
-                match('[');
-                expression();
-                match(']');
-                match('=');
-                expression();
-                match(';');
-            }
-            else
-            {
-                error("Erro de sintaxe: statement inválido.");
-            }
-            break;
-        default:
+            match(PONTVIR);
+        }
+        else if (lToken->name == LCOLCH) // ID [ Expression ] = Expression ;
+        {
+            match(LCOLCH);
+            expression();
+            match(RCOLCH);
+            match(EQUAL);
+            expression();
+            match(PONTVIR);
+        }
+        else
+        {
             error("Erro de sintaxe: statement inválido.");
-            break;
+        }
+        break;
+    default:
+        error("Erro de sintaxe: statement inválido.");
+        break;
     }
 }
 
 void Parser::expression()
 {
-    switch(lToken->name)
+    switch (lToken->name)
     {
-        case INTEGER_LITERAL:
-            match(INTEGER_LITERAL);
-            break;
-        case TRUE:
-            match(TRUE);
-            break;
-        case FALSE:
-            match(FALSE);
-            break;
-        case ID:
-            match(ID);
-            if(lToken->name == '[') // ID [ Expression ]
+    case INTEGER_LITERAL:
+        match(INTEGER_LITERAL);
+        break;
+    case TRUE:
+        match(TRUE);
+        break;
+    case FALSE:
+        match(FALSE);
+        break;
+    case ID:
+        match(ID);
+        if (lToken->name == LCOLCH) // ID [ Expression ]
+        {
+            match(LCOLCH);
+            expression();
+            match(RCOLCH);
+        }
+        else if (lToken->name == PONTO) // ID . length | ID . ID ( ( Expression (, Expression)* )? )
+        {
+            match(PONTO);
+            if (lToken->name == LENGTH)
             {
-                match('[');
-                expression();
-                match(']');
+                match(LENGTH);
             }
-            else if(lToken->name == '.') // ID . length | ID . ID ( ( Expression (, Expression)* )? )
-            {
-                match('.');
-                if(lToken->name == LENGTH)
-                {
-                    match(LENGTH);
-                }
-                else if(lToken->name == ID)
-                {
-                    match(ID);
-                    match('(');
-                    if(lToken->name != ')')
-                    {
-                        expression();
-                        while(lToken->name == ',')
-                        {
-                            match(',');
-                            expression();
-                        }
-                    }
-                    match(')');
-                }
-                else
-                {
-                    error("Erro de sintaxe: ID inválido.");
-                }
-            }
-            break;
-        case THIS:
-            match(THIS);
-            break;
-        case NEW:
-            match(NEW);
-            if(lToken->name == INT)
-            {
-                match(INT);
-                match('[');
-                expression();
-                match(']');
-            }
-            else if(lToken->name == ID)
+            else if (lToken->name == ID)
             {
                 match(ID);
-                match('(');
-                match(')');
+                match(LPAREN);
+                if (lToken->name != RPAREN)
+                {
+                    expression();
+                    while (lToken->name == VIRGULA)
+                    {
+                        match(VIRGULA);
+                        expression();
+                    }
+                }
+                match(RPAREN);
             }
             else
             {
-                error("Erro de sintaxe: operação NEW inválida.");
+                error("Erro de sintaxe: ID inválido.");
             }
-            break;
-        case NOT:
-            match(NOT);
+        }
+        break;
+    case THIS:
+        match(THIS);
+        break;
+    case NEW:
+        match(NEW);
+        if (lToken->name == INT)
+        {
+            match(INT);
+            match(LCOLCH);
             expression();
-            break;
-        case '(':
+            match(RCOLCH);
+        }
+        else if (lToken->name == ID)
+        {
+            match(ID);
             match(LPAREN);
-			expression();
-			match(RPAREN);
-			break;
+            match(RPAREN);
+        }
+        else
+        {
+            error("Erro de sintaxe: operação NEW inválida.");
+        }
+        break;
+    case NOT:
+        match(NOT);
+        expression();
+        break;
+    case LPAREN:
+        match(LPAREN);
+        expression();
+        match(RPAREN);
+        break;
+    }
+}
 
 void Parser::op()
 {
-    if (lToken->name == AND) {
+    if (lToken->name == AND)
+    {
         match(AND);
-    } else if (lToken->name == LT) {
+    }
+    else if (lToken->name == LT)
+    {
         match(LT);
-    } else if (lToken->name == GT) {
+    }
+    else if (lToken->name == GT)
+    {
         match(GT);
-    } else if (lToken->name == EQUAL) {
+    }
+    else if (lToken->name == EQUAL)
+    {
         match(EQUAL);
-    } else if (lToken->name == NOT_EQUAL) {
+    }
+    else if (lToken->name == NOT_EQUAL)
+    {
         match(NOT_EQUAL);
-    } else if (lToken->name == ADD) {
+    }
+    else if (lToken->name == ADD)
+    {
         match(ADD);
-    } else if (lToken->name == SUB) {
+    }
+    else if (lToken->name == SUB)
+    {
         match(SUB);
-    } else if (lToken->name == MUL) {
+    }
+    else if (lToken->name == MUL)
+    {
         match(MUL);
-    } else if (lToken->name == DIV) {
+    }
+    else if (lToken->name == DIV)
+    {
         match(DIV);
-    } else {
+    }
+    else
+    {
         error("Operador esperado");
     }
 }
 
-void
-Parser::error(string str)
+void Parser::error(string str)
 {
-	cout << "Linha " << scanner->getLine() << ": " << str << endl;
+    cout << "Linha " << scanner->getLine() << ": " << str << endl;
 
-	exit(EXIT_FAILURE);
+    exit(EXIT_FAILURE);
 }
